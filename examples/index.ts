ae54028ffbe97f614f9f90b91563107422c21e38
Mathieu TUDISCO <dev@mathieutu.ovh>
2019-01-10 18:47:02 +0100

    v0.0.3

--- examples/index.ts
+++ examples/index.ts
@@ -1,37 +1,39 @@
 import { Mailer } from 'nodemailer-react'
 import Password from './Password'
 
-import { Mailer as MailerTS } from '../dist'
-
-const mailerConfig = {
-  from: {
-    name: 'mathieutu',
-    email: 'dev@mathieutu.ovh',
-  },
-  transport: {
-    host: 'smtp.example.com',
-    secure: true,
-    auth: {
-      user: 'username',
-      pass: 'password',
+export default function sendEmail() {
+  const mailerConfig = {
+    from: {
+      name: 'mathieutu',
+      email: 'dev@mathieutu.ovh',
     },
-  },
+    transport: {
+      host: 'smtp.example.com',
+      secure: true,
+      auth: {
+        user: 'username',
+        pass: 'password',
+      },
+    },
+  }
+
+  const emailsList = {
+    pass: Password,
+  }
+
+  const mailer = new Mailer(mailerConfig, emailsList)
+
+  const template = 'pass'
+  const props = {
+    firstName: 'Mathieu',
+    lastName: 'Tudisco',
+    brand: 'Linkvalue',
+    newAccount: true,
+    password: Math.random().toString(36).substring(7),
+  }
+
+  mailer.send(template, props, {
+    to: 'foo@bar.fr',
+    attachments: [{ content: 'bar', filename: 'foo.txt' }],
+  })
 }
-
-const emailsList = {
-  pass: Password,
-}
-
-const mailer = new Mailer(mailerConfig, emailsList)
-
-mailer.send('pass', {}, {
-  to: 'foo@bar.fr',
-  attachments: [{ content: 'bar', filename: 'foo.txt' }],
-})
-
-const mailerTS = MailerTS(mailerConfig, emailsList)
-
-mailerTS.send('pass1', { firstName: '', }, {
-  to: 'foo@bar.fr',
-  attachments: [{ content: 'bar', filename: 'foo.txt' }],
-})
