35b1771729ebde226f4b95c056faf8df484ad040
Mathieu TUDISCO <mathieu@tudisco.fr>
2021-08-26 12:14:25 +0200

    💚 Update doctype to html5 and refactor files.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
-import EmailService from './EmailService'
+import { createTransport, SendMailOptions, SentMessageInfo, Transporter } from 'nodemailer'
+import { Options as TransportOptions } from 'nodemailer/lib/smtp-transport'
+import { ReactElement } from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
 
-export const Mailer = EmailService
+const renderBody = <P>(body: ReactElement<P>): string => `<!DOCTYPE html>${renderToStaticMarkup(body)}`
 
-export default EmailService
+export type EmailConfig = {
+  transport: TransportOptions,
+  defaults?: SendMailOptions,
+}
+
+export type Email<Props> = (props: Props) => {
+  body: ReactElement<Props>,
+  subject: string,
+}
+
+export type EmailsList = {
+  [name: string]: Email<any>,
+}
+
+export const Mailer = <Emails extends EmailsList>(config: EmailConfig, emails: Emails) => {
+  const { transport: transportConfig, defaults } = config
+  const transport: Transporter = createTransport(transportConfig, defaults)
+
+  const sendEmail = <TemplateName extends keyof Emails>(
+    template: TemplateName,
+    props: Parameters<Emails[TemplateName]>[0],
+    options: SendMailOptions,
+  ): Promise<SentMessageInfo> => {
+    const { subject, body } = emails[template](props)
+
+    return transport.sendMail({ subject, html: renderBody(body), ...options })
+  }
+
+  return {
+    send: sendEmail,
+  }
+}
+
+export default Mailer
